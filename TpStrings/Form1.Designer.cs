5fba9ce89d4b5b0026bed2415ba5bfbe03b23eec
Usuario <Usuario@DESKTOP-OVC1UU1>
2023-09-05 14:30:04 -0300

    Agregar archivos de proyecto.

---
+++ TpStrings/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace TpStrings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.extraer1 = new System.Windows.Forms.TextBox();
+            this.EXTRAER = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.minuscula = new System.Windows.Forms.Button();
+            this.MAYUSCULA = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RESTA = new System.Windows.Forms.Button();
+            this.suma = new System.Windows.Forms.Button();
+            this.LONGITUD = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CERRAR = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.L3 = new System.Windows.Forms.Label();
+            this.L2 = new System.Windows.Forms.Label();
+            this.B1 = new System.Windows.Forms.Button();
+            this.T1 = new System.Windows.Forms.TextBox();
+            this.T3 = new System.Windows.Forms.TextBox();
+            this.T2 = new System.Windows.Forms.TextBox();
+            this.L1 = new System.Windows.Forms.Label();
+            this.B2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // extraer1
+            // 
+            this.extraer1.Location = new System.Drawing.Point(698, 479);
+            this.extraer1.Multiline = true;
+            this.extraer1.Name = "extraer1";
+            this.extraer1.Size = new System.Drawing.Size(158, 34);
+            this.extraer1.TabIndex = 51;
+            // 
+            // EXTRAER
+            // 
+            this.EXTRAER.Location = new System.Drawing.Point(517, 458);
+            this.EXTRAER.Name = "EXTRAER";
+            this.EXTRAER.Size = new System.Drawing.Size(175, 66);
+            this.EXTRAER.TabIndex = 50;
+            this.EXTRAER.Text = "EXTRAER LETRAS";
+            this.EXTRAER.UseVisualStyleBackColor = true;
+            this.EXTRAER.Click += new System.EventHandler(this.EXTRAER_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(668, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "VALOR DE LONGITUD:";
+            // 
+            // minuscula
+            // 
+            this.minuscula.Location = new System.Drawing.Point(238, 437);
+            this.minuscula.Name = "minuscula";
+            this.minuscula.Size = new System.Drawing.Size(180, 61);
+            this.minuscula.TabIndex = 48;
+            this.minuscula.Text = "minuscula";
+            this.minuscula.UseVisualStyleBackColor = true;
+            this.minuscula.Click += new System.EventHandler(this.minuscula_Click);
+            // 
+            // MAYUSCULA
+            // 
+            this.MAYUSCULA.Location = new System.Drawing.Point(52, 437);
+            this.MAYUSCULA.Name = "MAYUSCULA";
+            this.MAYUSCULA.Size = new System.Drawing.Size(180, 61);
+            this.MAYUSCULA.TabIndex = 47;
+            this.MAYUSCULA.Text = "MAYUSCULA";
+            this.MAYUSCULA.UseVisualStyleBackColor = true;
+            this.MAYUSCULA.Click += new System.EventHandler(this.MAYUSCULA_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "O NUMERO(B):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "O NUMERO(A):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, -32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "SUMA O RESTA";
+            // 
+            // RESTA
+            // 
+            this.RESTA.Location = new System.Drawing.Point(653, 383);
+            this.RESTA.Name = "RESTA";
+            this.RESTA.Size = new System.Drawing.Size(158, 49);
+            this.RESTA.TabIndex = 43;
+            this.RESTA.Text = "RESTA";
+            this.RESTA.UseVisualStyleBackColor = true;
+            this.RESTA.Click += new System.EventHandler(this.RESTA_Click);
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(653, 328);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(158, 49);
+            this.suma.TabIndex = 42;
+            this.suma.Text = "SUMA";
+            this.suma.UseVisualStyleBackColor = true;
+            this.suma.Click += new System.EventHandler(this.suma_Click);
+            // 
+            // LONGITUD
+            // 
+            this.LONGITUD.AutoSize = true;
+            this.LONGITUD.Location = new System.Drawing.Point(726, 257);
+            this.LONGITUD.Name = "LONGITUD";
+            this.LONGITUD.Size = new System.Drawing.Size(0, 13);
+            this.LONGITUD.TabIndex = 41;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, -32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "CONCATENAR FRASES                     O";
+            // 
+            // CERRAR
+            // 
+            this.CERRAR.Location = new System.Drawing.Point(653, 140);
+            this.CERRAR.Name = "CERRAR";
+            this.CERRAR.Size = new System.Drawing.Size(158, 38);
+            this.CERRAR.TabIndex = 39;
+            this.CERRAR.Text = "CERRAR";
+            this.CERRAR.UseVisualStyleBackColor = true;
+            this.CERRAR.Click += new System.EventHandler(this.CERRAR_Click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(57, 355);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(175, 61);
+            this.B3.TabIndex = 38;
+            this.B3.Text = "CONCATENAR";
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // L3
+            // 
+            this.L3.AutoSize = true;
+            this.L3.Location = new System.Drawing.Point(81, 241);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(76, 13);
+            this.L3.TabIndex = 37;
+            this.L3.Text = "RESULTADO:";
+            // 
+            // L2
+            // 
+            this.L2.AutoSize = true;
+            this.L2.Location = new System.Drawing.Point(54, 153);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(109, 13);
+            this.L2.TabIndex = 36;
+            this.L2.Text = "INGRESE FRASE(B):";
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(238, 355);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(175, 61);
+            this.B1.TabIndex = 34;
+            this.B1.Text = "LONGITUD";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // T1
+            // 
+            this.T1.Location = new System.Drawing.Point(197, 56);
+            this.T1.Multiline = true;
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(384, 82);
+            this.T1.TabIndex = 33;
+            // 
+            // T3
+            // 
+            this.T3.Location = new System.Drawing.Point(197, 238);
+            this.T3.Multiline = true;
+            this.T3.Name = "T3";
+            this.T3.Size = new System.Drawing.Size(384, 82);
+            this.T3.TabIndex = 32;
+            // 
+            // T2
+            // 
+            this.T2.Location = new System.Drawing.Point(197, 150);
+            this.T2.Multiline = true;
+            this.T2.Name = "T2";
+            this.T2.Size = new System.Drawing.Size(384, 82);
+            this.T2.TabIndex = 31;
+            // 
+            // L1
+            // 
+            this.L1.AutoSize = true;
+            this.L1.Location = new System.Drawing.Point(54, 89);
+            this.L1.Margin = new System.Windows.Forms.Padding(12, 0, 12, 0);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(120, 13);
+            this.L1.TabIndex = 35;
+            this.L1.Text = "INGRESAR FRASE (A):";
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(419, 355);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(175, 61);
+            this.B2.TabIndex = 30;
+            this.B2.Text = "LIMPIAR";
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImage = global::TpStrings.Properties.Resources.wallpaperbetter_com_1920x1080;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(933, 618);
+            this.Controls.Add(this.extraer1);
+            this.Controls.Add(this.EXTRAER);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.minuscula);
+            this.Controls.Add(this.MAYUSCULA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RESTA);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.LONGITUD);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CERRAR);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.T1);
+            this.Controls.Add(this.T3);
+            this.Controls.Add(this.T2);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.B2);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "TRABAJO 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox extraer1;
+        private System.Windows.Forms.Button EXTRAER;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button minuscula;
+        private System.Windows.Forms.Button MAYUSCULA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button RESTA;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.Label LONGITUD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CERRAR;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Label L3;
+        private System.Windows.Forms.Label L2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.TextBox T1;
+        private System.Windows.Forms.TextBox T3;
+        private System.Windows.Forms.TextBox T2;
+        private System.Windows.Forms.Label L1;
+        private System.Windows.Forms.Button B2;
+    }
+}
+
---
+++ TpStrings/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TpStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+
+            string separar = " ";
+
+            T3.Text = T1.Text + separar + T2.Text;
+
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            int caracteres = 0;
+            caracteres = T3.Text.Length;
+
+            LONGITUD.Text = caracteres.ToString();
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            T1.Text = "";
+            T2.Text = "";
+            T3.Text = "";
+            LONGITUD.Text = " ";
+            extraer1.Text = "";
+        }
+
+        private void MAYUSCULA_Click(object sender, EventArgs e)
+        {
+
+            T3.Text = T3.Text.ToUpper();
+        }
+
+        private void minuscula_Click(object sender, EventArgs e)
+        {
+            T3.Text = T3.Text.ToLower();
+        }
+
+        private void EXTRAER_Click(object sender, EventArgs e)
+        {
+            T3.Text = T3.Text.Replace(extraer1.Text, "");
+        }
+
+        private void RESTA_Click(object sender, EventArgs e)
+        {
+            int a, b, restar;
+
+            a = int.Parse(T1.Text);
+            b = int.Parse(T2.Text);
+            restar = a - b;
+
+
+            T3.Text = restar.ToString();
+
+        }
+
+        private void suma_Click(object sender, EventArgs e)
+        {
+            int a, b, sumar;
+
+            a = int.Parse(T1.Text);
+            b = int.Parse(T2.Text);
+            sumar = a + b;
+
+
+            T3.Text = sumar.ToString();
+        }
+
+        private void CERRAR_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
